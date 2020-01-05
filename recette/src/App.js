87e58bd6a104689ce31893d5d52bb2c27bf62f80
imtiazraqib <imtiazud@ualberta.ca>
2020-01-05 14:02:40 -0700

    Able to retrieve data from Edamam API

--- recette/src/App.js
+++ recette/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="searchForm">
+        <input className="searchBar" type="text"/>
+        <button className="searchButton" type="submit">Search</button>
+      </form>
     </div>
     
-  )
+  );
 }
 
 export default App;

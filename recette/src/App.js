7b0297075be3649b2389d9e5b5b4d1004eacde1a
imtiazraqib <imtiazud@ualberta.ca>
2020-01-05 19:52:58 -0700

    Able to search for new recipes

--- recette/src/App.js
+++ recette/src/App.js
@@ -7,22 +7,41 @@ const App = () => {
   // using States to update the returned json array of recipes fro the API call
   const [recipes, setRecipes] = useState([]);
 
+  // Using state to update the search bar with input
+  const [search, setSearch] = useState('');
+
+  // Saving the state of the query once Search button is pressed
+  const [query, setQuery] = useState('Meat')
+
   // useEffect is used to only run getRecipes() once when the page first loads
+  // and only when query changes and the search button is pressed
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   // Asychronous call to Edamam to retrieve searched token data
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
   }
 
+  // Updating the state of searches
+  const updateSearchBar = e => {
+    setSearch(e.target.value);
+  }
+
+  // Setting the completed query state once the search button is pressed
+  const getQueryRecipe = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <div className="App">
-      <form className="searchForm">
-        <input className="searchBar" type="text"/>
+      <form onSubmit={getQueryRecipe} className="searchForm">
+        <input className="searchBar" type="text" value={search} onChange={updateSearchBar} />
         <button className="searchButton" type="submit">Search</button>
       </form>
       {recipes.map(recipe =>  (
